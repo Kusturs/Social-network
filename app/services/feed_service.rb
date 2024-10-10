f104244f17dc9ec717d6121af2a5706c6c8c2335
FusionNotOfficial <bags4circles@gmail.com>
2024-10-10 11:20:59 +0300

    me

--- app/services/feed_service.rb
+++ app/services/feed_service.rb
@@ -6,6 +6,23 @@ class FeedService
   end
 
   def call
-    Post.includes(:user).where(user: @user.following).order(created_at: :desc)
+    {
+      user_info: user_info,
+      feed_posts: feed_posts
+    }
+  end
+
+  private
+
+  def user_info
+    {
+      followers_count: @user.followers_count,
+      following_count: @user.followed_count
+    }
+  end
+
+  def feed_posts
+    following_ids = @user.active_subscriptions.pluck(:followed_id)
+    Post.includes(:author).where(author_id: following_ids).order(created_at: :desc)
   end
 end
